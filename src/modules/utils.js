a650b7ccbf1e0cd12097f82ec2c31576a0d58436
shreeup <shree912@yahoo.com>
2023-06-26 22:10:16 -0700

    Update utils.js

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,1 +1,20 @@
 
+
+export async function getToken(clientId, clientSecret) {
+
+  const b64conv = btoa(clientId + ':' + clientSecret);
+  return await fetch("https://accounts.spotify.com/api/token", {
+    body: "grant_type=client_credentials",
+    headers: {
+      "Authorization": "Basic " + b64conv,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    method: "POST"
+  }).then(
+
+    data => data.json().then(json => {
+      return json['access_token']
+    }),
+    err => console.log('something went wrong', err)
+  );
+}
